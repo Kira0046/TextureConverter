725ed5faed1f03a07a3051f78f68cd86caf13517
Kira003 <k021g1248@g.neec.ac.jp>
2023-10-10 14:59:48 +0900

    2-4

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
+#include <DirectXTex.h>
+#include "TextureConverter.h"
+
+enum Argument {
+	kApplicationPath,
+	kFilePath,
+
+	NumArgument
+};
 
 int main(int argc,char* argv[]) {
 	
@@ -8,6 +17,17 @@ int main(int argc,char* argv[]) {
 
 		printf("\n");
 	}
+
+	assert(argc >= NumArgument);
+
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	TextureConverter converter;
+
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	CoUninitialize();
 
 	system("pause");
 	return 0;
