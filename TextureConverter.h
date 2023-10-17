ef1700b07b5632ee2b4279ad98ff17767c419e79
Kira003 <k021g1248@g.neec.ac.jp>
2023-10-17 15:09:42 +0900

    2-5

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,14 +1,48 @@
 #pragma once
 #include <string>
+#include <DirectXTex.h>
 
 class TextureConverter
 {
 public:
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath"></param>
 	void ConvertTextureWICToDDS(const std::string& filePath);
 
 private:
+	/// <summary>
+	/// テクスチャ読み込み
+	/// </summary>
+	/// <param name="filePath"></param>
 	void LoadWICTextureFromFile(const std::string& filePath);
 
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString"></param>
+	/// <returns></returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離
+	/// </summary>
+	/// <param name="filePath"></param>
+	void SeparateFilePath(const std::wstring& filePath);
+	
+	void SaveDDSTextureToFile();
+
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
 
